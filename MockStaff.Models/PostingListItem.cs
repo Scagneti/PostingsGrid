034e2cbe411900c7121a01281b0cf27d4eea5fd0
Zach Nelson <zachn@knowledgeservices.com>
2018-10-17 10:44:23 -0400

    created services and api controller

---
+++ MockStaff.Models/PostingListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockStaff.Models
+{
+	public class PostingListItem
+	{
+		public int PostingId { get; set; }
+		public string Title { get; set; }
+		public DateTimeOffset CreatedDate { get; set; }
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+}
